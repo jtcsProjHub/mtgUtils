7015b451c316b1cdd52cbb34499cebc14a6da91a
John Cameron <gearsareshiny@gmail.com>
2022-12-19 18:56:00 -0500

    Working coverter

--- Dragon2Moxfield.py
+++ Dragon2Moxfield.py
@@ -9,7 +9,7 @@ from tkinter import filedialog
 # Tacks on a .csv extension for good measure.
 def prep_csv_file(file_path_and_name):
     try:
-        csvfile = open(str(file_path_and_name) + ".csv", 'w', newline='', encoding='ascii')
+        csvfile = open(str(file_path_and_name) + "_moxfield.csv", 'w', newline='', encoding='utf-8')
     except (FileNotFoundError, IOError, FileExistsError):
         print("Error opening CSV file. Check permissions.")
         return -1
@@ -18,22 +18,9 @@ def prep_csv_file(file_path_and_name):
 
     # Prepare to write out the Moxfield CSV format
     # Count,Tradelist Count,Name,Edition,Condition,Language,Foil,Tags,Last Modified,Collector Number
-    csv_header = ["Count","Tradelist","Count","Name","Edition","Condition","Language","Foil","Tags","Last Modified","Collector Number"]
+    csv_header = ["Count","Tradelist","Name","Edition","Condition","Language","Foil","Tags","Last Modified","Collector Number"]
     csv_results.writerow(csv_header)
     return csv_results
-
-def csv_line_write(card_data, csv_file):
-    # Count,Tradelist Count,Name,Edition,Condition,Language,Foil,Tags,Last Modified,Collector Number
-    foil = ''
-    is_pinned = ''
-    signed = ''
-    set_uid = ''
-    card_condition = 'NM'
-    card_language = 'en'
-    card_set_code = card_data['cardSetId'].upper()
-
-    csv_file.writerow([card_data['cardQuantity'], card_data['cardName'], foil, is_pinned, signed, set_uid\
-        , card_set_code, card_data['cardSetNumber'], card_language, card_condition,'',''])
 
 # Open the reference file from disk. This is intended to be a Dragon Shield
 # CSV formatted text file, but no checks are done here.
@@ -46,19 +33,21 @@ if csvOutputFile == -1:
 
 # Look through each of the lines in the file and rearrange the data for the output file
 # Count,Tradelist Count,Name,Edition,Condition,Language,Foil,Tags,Last Modified,Collector Number
-tradelist_count = '0'
-condition = 'Near Mint'
-language = 'English'
-tags = ''
+TRADELIST_COUNT = '0'
+CONDITION = 'Near Mint'
+TAGS = ''
 today = date.today()
 last_modified = today.strftime("%m/%d/%y")
 
 try:
-    with open(file_path, newline='') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            print(row)
-except (FileExistsError, FileNotFoundError, IOError, csv.Error) as e:
+    with open(file_path, newline='', encoding='utf-8') as csvfile:
+        card_data_all = csv.DictReader(csvfile)
+        for card_entry in card_data_all:
+            foil = 'Foil' if card_entry["Printing"] == "Foil" else ''
+            csvOutputFile.writerow([card_entry["Quantity"], TRADELIST_COUNT, card_entry["Card Name"],\
+                card_entry["Set Code"].lower(), CONDITION, card_entry["Language"], foil, TAGS,\
+                last_modified, card_entry["Card Number"]])
+except (FileExistsError, FileNotFoundError, IOError, csv.Error, KeyError) as e:
     print("Error opening or processing the CSV file.")
     print(e)
     
