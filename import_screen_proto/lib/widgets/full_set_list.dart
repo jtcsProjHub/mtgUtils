b9e836fff3cf3655dbaaf5324cd295b1a18af20a
John Cameron <gearsareshiny@gmail.com>
2023-02-19 08:29:22 -0500

    Integrated the multi-select list package to start helping with aesthetics

--- import_screen_proto/lib/widgets/full_set_list.dart
+++ import_screen_proto/lib/widgets/full_set_list.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:import_screen_proto/scryfall_singleton.dart';
 import 'package:scryfall_api/scryfall_api.dart';
+import 'package:flutter_multi_select_items/flutter_multi_select_items.dart';
 
 class FullSetList extends StatefulWidget {
   const FullSetList({super.key});
@@ -11,7 +12,7 @@ class FullSetList extends StatefulWidget {
 
 class _FullSetListState extends State<FullSetList> {
   final _scryfallSingletonRef = ScryfallSingleton();
-  final ScrollController _controller = ScrollController();
+  final MultiSelectController<String> _controller = MultiSelectController();
   PaginableList<MtgSet> _fullSetData = PaginableList(data: [], hasMore: false);
 
   @override
@@ -55,30 +56,35 @@ class _FullSetListState extends State<FullSetList> {
   // Actually builds out the scrollable list from the paginated set data
   Widget _buildListView(PaginableList<MtgSet> setData) {
     _fullSetData = setData;
-    return Container(
-        height: 500,
-        child: ListView.builder(
-          physics: const AlwaysScrollableScrollPhysics(),
-          controller: _controller,
-          itemCount: _fullSetData.length,
-          shrinkWrap: true,
-          prototypeItem: ListTile(
-              title: Row(
-            children: [
-              Text(_fullSetData.data.first.name),
-              Text(_fullSetData.data.first.code)
-            ],
-          )),
-          itemBuilder: (context, index) {
-            return ListTile(
-              title: Row(
-                children: [
-                  Text(_fullSetData.data[index].name),
-                  Text(_fullSetData.data[index].code)
-                ],
-              ),
-            );
-          },
-        ));
+    Widget scrollableList = Container(
+      height: 500,
+      child: MultiSelectCheckList(
+        textStyles: const MultiSelectTextStyles(
+            selectedTextStyle:
+                TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+        itemsDecoration: MultiSelectDecorations(
+            selectedDecoration:
+                BoxDecoration(color: Colors.indigo.withOpacity(0.8))),
+        listViewSettings: ListViewSettings(
+            separatorBuilder: (context, index) => const Divider(
+                  height: 0,
+                )),
+        controller: _controller,
+        items: List.generate(
+            _fullSetData.length,
+            (index) => CheckListCard(
+                value: _fullSetData[index].id,
+                title: Text(_fullSetData[index].name),
+                subtitle: Text(_fullSetData[index].code),
+                selectedColor: Colors.white,
+                checkColor: Colors.indigo,
+                checkBoxBorderSide: const BorderSide(color: Colors.blue),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5)))),
+        onChange: (allSelectedItems, selectedItem) {},
+      ),
+    );
+
+    return scrollableList;
   }
 }
