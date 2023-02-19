eba472dfa9a3d1f43b26d2d827babc134a785691
John Cameron <gearsareshiny@gmail.com>
2023-02-19 16:39:03 -0500

    Side list showing selections of sets to enforce on import

--- import_screen_proto/lib/widgets/full_set_list.dart
+++ import_screen_proto/lib/widgets/full_set_list.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:import_screen_proto/scryfall_singleton.dart';
 import 'package:scryfall_api/scryfall_api.dart';
@@ -12,8 +14,13 @@ class FullSetList extends StatefulWidget {
 
 class _FullSetListState extends State<FullSetList> {
   final _scryfallSingletonRef = ScryfallSingleton();
-  final MultiSelectController<String> _controller = MultiSelectController();
+  final MultiSelectController<String> _multiController =
+      MultiSelectController();
+  final ScrollController _controller = ScrollController();
   PaginableList<MtgSet> _fullSetData = PaginableList(data: [], hasMore: false);
+
+  // These are going to be strings of the indices. Easier that way.
+  List<String> _selectedSets = [];
 
   @override
   void initState() {
@@ -57,34 +64,69 @@ class _FullSetListState extends State<FullSetList> {
   Widget _buildListView(PaginableList<MtgSet> setData) {
     _fullSetData = setData;
     Widget scrollableList = Container(
-      height: 500,
-      child: MultiSelectCheckList(
-        textStyles: const MultiSelectTextStyles(
-            selectedTextStyle:
-                TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
-        itemsDecoration: MultiSelectDecorations(
-            selectedDecoration:
-                BoxDecoration(color: Colors.indigo.withOpacity(0.8))),
-        listViewSettings: ListViewSettings(
-            separatorBuilder: (context, index) => const Divider(
-                  height: 0,
-                )),
-        controller: _controller,
-        items: List.generate(
-            _fullSetData.length,
-            (index) => CheckListCard(
-                value: _fullSetData[index].id,
-                title: Text(_fullSetData[index].name),
-                subtitle: Text(_fullSetData[index].code),
-                selectedColor: Colors.white,
-                checkColor: Colors.indigo,
-                checkBoxBorderSide: const BorderSide(color: Colors.blue),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(5)))),
-        onChange: (allSelectedItems, selectedItem) {},
-      ),
+        height: 500,
+        width: 500,
+        child: MultiSelectCheckList(
+          textStyles: const MultiSelectTextStyles(
+              selectedTextStyle:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+          itemsDecoration: MultiSelectDecorations(
+              selectedDecoration:
+                  BoxDecoration(color: Colors.indigo.withOpacity(0.8))),
+          listViewSettings: ListViewSettings(
+              separatorBuilder: (context, index) => const Divider(
+                    height: 0,
+                  )),
+          controller: _multiController,
+          items: List.generate(
+              _fullSetData.length,
+              (index) => CheckListCard(
+                  value: index.toString(),
+                  title: Text(_fullSetData[index].name),
+                  subtitle: Text(_fullSetData[index].code),
+                  selectedColor: Colors.white,
+                  checkColor: Colors.indigo,
+                  checkBoxBorderSide: const BorderSide(color: Colors.blue),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5)))),
+          onChange: (allSelectedItems, selectedItem) {
+            setState(() {
+              _selectedSets = allSelectedItems;
+            });
+          },
+        ));
+
+    Widget selectedSetsList = Container(
+        height: 500,
+        width: 500,
+        child: ListView.builder(
+          physics: const AlwaysScrollableScrollPhysics(),
+          controller: _controller,
+          itemCount: _selectedSets.length,
+          shrinkWrap: true,
+          prototypeItem: ListTile(
+              title: Row(
+            children: [
+              Text(_fullSetData.data.first.name),
+              Text(_fullSetData.data.first.code)
+            ],
+          )),
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Row(
+                children: [
+                  Text(_fullSetData.data[int.parse(_selectedSets[index])].name),
+                  Text(_fullSetData.data[int.parse(_selectedSets[index])].code)
+                ],
+              ),
+            );
+          },
+        ));
+
+    Widget limitToSetsPanel = Row(
+      children: [scrollableList, selectedSetsList],
     );
 
-    return scrollableList;
+    return limitToSetsPanel;
   }
 }
